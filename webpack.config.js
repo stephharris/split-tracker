e3e3fa6d3924adc163b616cabcf8d28bf8170092
Steph Harris <stephanie.harris@firstlook.org>
2020-12-29 14:38:13 -0500

    yayy

--- webpack.config.js
+++ webpack.config.js
@@ -2,12 +2,11 @@ const path = require('path');
 
 module.exports = {
   mode: 'production',
-  entry: './index.js',
+  entry: './src/index.js',
   output: {
     path: path.resolve(__dirname, 'dist'),
-    filename: 'bundle.js',
-    library: 'split-tracker',
-    libraryTarget: 'umd'
+    filename: 'index.js',
+    libraryTarget: 'commonjs2',
   },
   module: {
     rules: [
