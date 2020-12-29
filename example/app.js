9196e0bd266ce28cb6be9858bbfbbedc5333caeb
Steph Harris <stephanie.harris@firstlook.org>
2020-12-29 13:01:22 -0500

    adding in webpack & bundle

--- example/app.js
+++
@@ -1,7 +0,0 @@
-const { convertMinutesToSeconds,
-	convertSecondsToMinutes,
-	calculateSplitByMileTime,
-	calculateSplitByKilometerTime,
-	convertMinutesInDecimalsToMMSS } = require('split-tracker');
-
-console.log(calculateSplitByMileTime('08:15', 200))
---
+++ webpack.config.js
@@ -0,0 +1,24 @@
+const path = require('path');
+
+module.exports = {
+  mode: 'production',
+  entry: './index.js',
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'bundle.js',
+    library: 'split-tracker',
+    libraryTarget: 'umd'
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js?$/,
+        exclude: /(node_modules)/,
+        use: 'babel-loader',
+      },
+    ],
+  },
+  resolve: {
+    extensions: ['.js'],
+  },
+};
